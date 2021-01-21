65cf014426649fd2d98673c961fcc4c21e1d6fd2
keisuke fujii <fujiisoup@gmail.com>
2021-01-22 06:35:23 +0900

    improve setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ from __future__ import print_function
 import re
 from numpy.distutils.core import setup, Extension
 
+import platform
+from ctypes.util import find_library
+
 # load version form _version.py
 VERSIONFILE = "py3nj/_version.py"
 verstrline = open(VERSIONFILE, "rt").read()
@@ -14,6 +17,34 @@ if mo:
 else:
     raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
 
+
+# If macOS, use ctypes.util.find_library to determine if OpenMP is available.
+openmp = None
+if openmp is None and platform.system() == 'Darwin':  # Check if macOS
+    if find_library("omp") != None:
+        openmp = True
+        print("libOMP found, building with OpenMP")
+    else:
+        print("libOMP not found, building without OpenMP")
+elif openmp in (None, True, "True", "true"):
+    openmp = True
+elif openmp in (False, "False", "false"):
+    openmp = False
+    print("Building without OpenMP")
+else:
+    raise ValueError("Env var ompy_OpenMP must be either True or False "
+                     "(or not set); use eg. 'export ompy_OpenMP=False'"
+                     f"Now it is: {openmp}")
+
+extra_compile_args = ["-O3", "-ffast-math", "-march=native"]
+extra_link_args = []
+if openmp and platform.system() == 'Darwin':
+    extra_compile_args.insert(-1, "-Xpreprocessor -fopenmp")
+    extra_link_args.insert(-1, "-lomp")
+elif openmp:
+    extra_compile_args.insert(-1, "-fopenmp")
+    extra_link_args.insert(-1, "-fopenmp")
+    
 # fortran extension
 ext = Extension(
     name="py3nj._wigner",
@@ -25,7 +56,8 @@ ext = Extension(
         "fortran/drc3jj.f",
         "fortran/drc6j.f",
     ],
-    extra_compile_args=["-fopenmp"],
+    extra_compile_args=extra_compile_args,
+    extra_link_args=extra_link_args,
 )
 
 # module
