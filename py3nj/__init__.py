f36ea60021aaa92149a3b91922bce6210107b998
Maks Zachary Walewski <maks.walewski@gmail.com>
2023-03-28 18:51:44 +0200

    .lib/ added to dll locations in __init__.py

--- py3nj/__init__.py
+++ py3nj/__init__.py
@@ -7,6 +7,7 @@ extra_dll_dir = os.path.join(os.path.dirname(__file__), ".libs")
 if sys.platform == "win32" and os.path.isdir(extra_dll_dir):
     os.environ.setdefault("PATH", "")
     os.environ["PATH"] += os.pathsep + extra_dll_dir
+    os.add_dll_directory(extra_dll_dir)
 
 from ._version import __version__
 
